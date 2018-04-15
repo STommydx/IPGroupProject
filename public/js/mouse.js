6e3be5d88669b6b5cbbcd6b2a583d983fb0c3dd3
Sum_01 <35803102+jask110a@users.noreply.github.com>
2018-04-15 18:28:25 +0800

    smoother in mouse animation

--- public/js/mouse.js
+++ public/js/mouse.js
@@ -7,16 +7,16 @@ function animateCircles (event){
 			circle.setAttribute("class", "circle");
 			document.body.appendChild(circle);
 
-			circle.style.left = event.clientX + 'px';
-			circle.style.top = event.clientY + 'px';
+			circle.style.left = event.clientX +5+ 'px';
+			circle.style.top = event.clientY +5+ 'px';
 
 			var color = colors[Math.floor(Math.random() * colors.length)];
 			circle.style.borderColor = color;
 
 			circle.style.transition = "all 0.2s linear 0s";
 
-			circle.style.left = circle.offsetLeft +40 + 'px';
-			circle.style.top = circle.offsetTop +20+ 'px';
+			circle.style.left = circle.offsetLeft +40+ 'px';
+			circle.style.top = circle.offsetTop +40+ 'px';
 
 
 			circle.style.opacity = 0;
